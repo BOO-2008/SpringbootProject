2c1001e52b8c7da8841e05ca8786f3fe10ddffed
Sakthivel Srinivasan <sakthivelsrinivasan@192.168.1.9>
2022-11-27 21:26:08 +0530

    First Commit

---
+++ src/main/java/com/demo/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+	@RequestMapping(value="/")
+	public ModelAndView loginPage() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("LoginPage");
+		return mv;
+	}
+
+}
